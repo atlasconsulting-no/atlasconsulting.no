c73a8fc1518a269c72f2d3fbfae2bb3ca1a86d58
Sander Knutsen <sander@metasolutions.no>
2023-10-31 19:45:46 +0100

    added logo text next to logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,7 +42,10 @@ const Navbar = () => {
                 <div className='navbar-content flex fw-7'>
                     <div className='brand-and-toggler flex flex-between w-100'>
                         <Link to="/" className='navbar-brand' onClick={moveToTop}>
+                        <div className='logo-and-text flex'>
                             <img src={images.logo_img} alt="" onClick={moveToTop} />
+                            <div>Atlas Consulting</div>
+                        </div>
                         </Link>
                         <div type="button" className={`hamburger-menu ${navToggle ? 'hamburger-menu-change' : ""}`} onClick={navHandler}>
                             <div className='bar-top'></div>
