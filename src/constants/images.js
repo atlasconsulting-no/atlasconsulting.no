5bdc623870ec3a51c4c342ab7d5969d8ddba6a80
belinox <bernardo.lino.oliveira@gmail.com>
2023-10-31 19:41:36 +0100

    Minor cleanup and draft logo

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,23 +1,11 @@
 import header_main_img from "../assets/images/AdobeStock_339503989.jpeg";
 import about_main_img from "../assets/images/AdobeStock_339503989.jpeg";
-import form_main_img from "../assets/images/form_main_img.jpg";
-import professionals_main_img from "../assets/images/professionals_main_img.jpg";
-import video_cover_img from "../assets/images/video_cover_img.jpg";
-import customer_img_1 from "../assets/images/customer_img_1.jpg";
-import customer_img_2 from "../assets/images/customer_img_2.jpg";
-import customer_img_3 from "../assets/images/customer_img_3.jpg";
-import customer_img_4 from "../assets/images/customer_img_4.jpg";
-import customer_img_5 from "../assets/images/customer_img_5.jpg";
-import customer_img_6 from "../assets/images/customer_img_6.jpg";
 
-import portfolio_img_1 from "../assets/images/portfolio_img_1.jpg";
-import portfolio_img_2 from "../assets/images/portfolio_img_2.jpg";
-import portfolio_img_3 from "../assets/images/portfolio_img_3.jpg";
 import bernardo_img from "../assets/images/Bernardo.jpg";
 import sander_img from "../assets/images/Sander.jpg";
 
-import logo_img from "../assets/images/logo-light.png";
+import logo_img from "../assets/images/logo.png";
 
-const images = {bernardo_img, sander_img, header_main_img, about_main_img, form_main_img, professionals_main_img, video_cover_img, customer_img_1, customer_img_2, customer_img_3, customer_img_4, customer_img_5, customer_img_6, portfolio_img_1, portfolio_img_2, portfolio_img_3, logo_img};
+const images = {bernardo_img, sander_img, header_main_img, about_main_img, logo_img};
 
 export default images;
