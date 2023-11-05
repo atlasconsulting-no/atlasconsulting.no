7c0105f5be0b2d2984372138dd9c58ee746f7da5
belinox <bernardo.lino.oliveira@gmail.com>
2023-11-05 20:39:32 +0100

    Revert "Tried implementing deploy script"
    This reverts commit d61f1170752907effc85fcc1d3349b76addc73f7.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Header from './components/Header/Header';
 import "./assets/js/script";
 import Services from './components/Services/Services';
 import About from './components/About/About';
+import Qualities from './components/Qualities/Qualities';
+import Features from './components/Features/Features';
+import Portfolio from './components/Portfolio/Portfolio';
 import Consultants from './components/Consultants/Consultants';
+import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -12,7 +16,11 @@ function App() {
       <Header id='home'/>
       <About id='about'/>
       <Services id='services'/>
+      {/* <Qualities /> */}
+      {/* <Features /> */}
+      {/* <Portfolio /> */}
       <Consultants id='consultants'/>
+      {/* <Contact /> */}
       <Footer />
     </div>
   );
