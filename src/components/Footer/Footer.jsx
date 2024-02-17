d32d602d36c0579adbed229a4a16c0583675dd93
belinox <bernardo.lino.oliveira@gmail.com>
2024-02-17 09:50:58 +0100

    Realigned consultant grid

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className='footer bg-black flex flex-center' id = "footer">
         <div className='container flex flex-center w-100'>
             <div className='grid footer-content text-center'>
-                <span className='text'>post@atlasconsulting.no</span>    {/* must be bought or changed */}
+                <span className='text'>bernardo@atlasconsulting.no</span>    {/* must be bought or changed */}
 
                 <span className='text'>Org nr: 927 606 313 </span>
                 <span className='text'> &copy; Atlas Consulting AS</span>
